df6a4688d70c8b2c281876da2a5740c032b2e397
Jean Carlos <jeancarlosrecio@gmail.com>
2024-04-23 09:47:14 -0500

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NDE0NA/spectral.js?branch_slug=main&token=298edefb-0bd0-458e-a9e0-854c9ca7919d";
+export default { extends: ruleset };
